231d6bd002c6f89074907d54021881131d9c3db5
Rahul Singh <rkant225@gmail.com>
2021-11-02 20:04:34 +0530

    Done code changes to update Resume and Add 2 new projects and updated experience to 3.9 years.

--- src/Components/About.js
+++ src/Components/About.js
@@ -34,7 +34,7 @@ const About = () => {
                     </div>
                     <div className="about-text">
                         <div className="i-am-header-under-about">I am Rahul and I'm a <span className="typing-2"></span></div>
-                            I have total 3.5 years of experience in IT/Software industry. My area of expertise is React JS, which is frontend library developed and maintained by Facebook, and I have 3 years of experience in React JS. Worked with UI technologies like HTML5, JavaScript, CSS. Hands on experience in Redux (state management). Apart from this I have good hands on experience in Python and Python based Machine Learning model building to solve business problems. 
+                            I have total 3.9 years of experience in IT/Software industry. My area of expertise is React JS, which is frontend library developed and maintained by Facebook, and I have 3 years of experience in React JS. Worked with UI technologies like HTML5, JavaScript, CSS. Hands on experience in Redux (state management). Apart from this I have good hands on experience in Python and Python based Machine Learning model building to solve business problems. 
 
                             <div id="extra-about-text" className="about-text" style={{width : '100%'}}>
                                 <br/>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -23,7 +23,7 @@ const Projects = () => {
                     <div className="project-description">
                         This is a simple React JS based project which I created recently to learn basic CSS and CSS Animations. It covers props, state and event-handling.
                     </div>
-                    <a href="https://rkant225.github.io/tic-tac-toe-react/" target="_blank">
+                    <a href="https://rkant225.github.io/tic-tac-toe-react/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -38,7 +38,7 @@ const Projects = () => {
                     <div className="project-description">
                         This application demonstrate the concept of useEffect and useState React hooks. It fetches data from open API provided by indian Govt.
                     </div>
-                    <a href="https://rkant225.github.io/covid-vaccine-avalibility/" target="_blank">
+                    <a href="https://rkant225.github.io/covid-vaccine-avalibility/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -53,7 +53,7 @@ const Projects = () => {
                     <div className="project-description">
                         This is pure HTML and CSS based prohect and I have used React JS to implementing Routing and wraper of html and css. As of now there are 5-6 animations in this App.
                     </div>
-                    <a href="https://rkant225.github.io/animations/" target="_blank">
+                    <a href="https://rkant225.github.io/animations/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -68,7 +68,7 @@ const Projects = () => {
                     <div className="project-description">
                         This is another React based application which demonstrate different concepts such as Redux, Routing, Axiox, JWT Authorization, login, Material-UI, Modal. 
                     </div>
-                    <a href="https://place-book-frontend.herokuapp.com/" target="_blank"> 
+                    <a href="https://place-book-frontend.herokuapp.com/" target="_blank" rel="noreferrer"> 
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -84,7 +84,7 @@ const Projects = () => {
                     <div className="project-description">
                         This is a backend server for my <a href="https://place-book-frontend.herokuapp.com/">PlaceBook App</a>. It includes different cool tech such as Node JS, Express JS, Multer, Cors, Middlewares, Routing, JWT token.
                     </div>
-                    <a href="https://place-book-backend.herokuapp.com/" target="_blank">
+                    <a href="https://place-book-backend.herokuapp.com/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -99,7 +99,7 @@ const Projects = () => {
                     <div className="project-description">
                         This is basic react based app that has oOath-2 based login system using Google. It's backend is created by me using Node JS, Routing, Passport JS, SendGrid.
                     </div>
-                    <a href="https://sheltered-tundra-24258.herokuapp.com/" target="_blank">
+                    <a href="https://sheltered-tundra-24258.herokuapp.com/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -114,7 +114,7 @@ const Projects = () => {
                     <div className="project-description">
                         This App demonstrate the concept of fetching data asynchronously from third party server or any endpoint. API returns the data of India.
                     </div>
-                    <a href="https://rkant225.github.io/COVID_19_tracker_india" target="_blank">
+                    <a href="https://rkant225.github.io/COVID_19_tracker_india" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -129,7 +129,7 @@ const Projects = () => {
                     <div className="project-description">
                     This App demonstrate the concept of fetching data asynchronously from third party server or any endpoint. API returns the data of World.
                     </div>
-                    <a href="https://rkant225.github.io/COVID_19_tracker_WorldWide/" target="_blank">
+                    <a href="https://rkant225.github.io/COVID_19_tracker_WorldWide/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -144,7 +144,7 @@ const Projects = () => {
                     <div className="project-description">
                         This is a simple HTML and CSS based static page. It demonstrate the basic concepts of CSS and HTML. It helped me to learn basic CSS.
                     </div>
-                    <a href="https://rkant225.github.io/sample_restaurant/" target="_blank">
+                    <a href="https://rkant225.github.io/sample_restaurant/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -159,7 +159,7 @@ const Projects = () => {
                     <div className="project-description">
                         This is the App which I have created when I started lerning React. This App impliments the Redux from scratch. (store, reducer, action, dispatcher)
                     </div>
-                    <a href="https://rkant225.github.io/Redux_implementation_from_scratch/" target="_blank">
+                    <a href="https://rkant225.github.io/Redux_implementation_from_scratch/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -174,7 +174,7 @@ const Projects = () => {
                     <div className="project-description">
                         I have created this App to manage the customer for my own shop. I have created frontend as well as backend for this app. React JS, Node JS, Material-UI.
                     </div>
-                    <a href="http://customer-mgmt-sys-frontend.herokuapp.com/customer" target="_blank">
+                    <a href="http://customer-mgmt-sys-frontend.herokuapp.com/customer" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -189,7 +189,7 @@ const Projects = () => {
                     <div className="project-description">
                         This is a simple game which I have built using React JS, CSS and Javascript. I got inspiration to build this app from Chrom's Dino-T-Rex game.
                     </div>
-                    <a href="https://rkant225.github.io/tom-and-jerry/" target="_blank">
+                    <a href="https://rkant225.github.io/tom-and-jerry/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -204,7 +204,7 @@ const Projects = () => {
                     <div className="project-description">
                         I have created this App to using CSS, HTML and React JS. This app displays the current time in both Analog and Digital clock. It uses the concept of degree rotation.
                     </div>
-                    <a href="https://rkant225.github.io/clock/" target="_blank">
+                    <a href="https://rkant225.github.io/clock/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -219,7 +219,7 @@ const Projects = () => {
                     <div className="project-description">
                         I have created this App to using CSS, HTML and React JS. This app enables user to bid their vehicle for booking. I have used Formik to validate form.
                     </div>
-                    <a href="https://vahak-vehicle-bidding-app.herokuapp.com/" target="_blank">
+                    <a href="https://vahak-vehicle-bidding-app.herokuapp.com/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
@@ -234,7 +234,37 @@ const Projects = () => {
                     <div className="project-description">
                         I have created this App to using CSS, HTML and React JS. This app enables user to Chat with there friends in group. App is based on socket.io
                     </div>
-                    <a href="https://my-whatsapp-app-chatty.netlify.app/" target="_blank">
+                    <a href="https://my-whatsapp-app-chatty.netlify.app/" target="_blank" rel="noreferrer">
+                        <div className="btn-view-project">View</div>
+                    </a>
+                </div>
+
+                <div className="project">
+                    <div className="project-icon">
+                        <i class="fa fa-atom"></i>
+                    </div>
+                    <div className="project-name">
+                        React-Redux Setup
+                    </div>
+                    <div className="project-description">
+                        I have created this App to setup React with Redux. This app contains the Store, Action, Reducer and Connect method.
+                    </div>
+                    <a href="https://rkant225.github.io/react_redux_setup/" target="_blank" rel="noreferrer">
+                        <div className="btn-view-project">View</div>
+                    </a>
+                </div>
+
+                <div className="project">
+                    <div className="project-icon">
+                        <i class="fa fa-braille"></i>
+                    </div>
+                    <div className="project-name">
+                        Star Patterns
+                    </div>
+                    <div className="project-description">
+                        I have created this app to learn the implementation of start patterns with help of 2 nested for-loop.
+                    </div>
+                    <a href="https://rkant225.github.io/star_patterns/" target="_blank" rel="noreferrer">
                         <div className="btn-view-project">View</div>
                     </a>
                 </div>
